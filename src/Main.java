1d509f7c3f18d3c094c295eaf54863d49a3a9f7e
unknown <GITHUB andyskl03@gmail.com>
2018-09-21 19:50:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+//    public static void main(String[] args) {
+//        for (int i = 0; i<10; i++ ){
+//            for (int j = 0; j<10; j++ ){
+//                System.out.printf("%5d" ,(i + 1) * (j + 1));
+//            }
+//            System.out.println();
+//        }
+public static void main(String[] args) {
+    int i=0;
+    for (i=2;i<21;i++)  System.out.println(" "+Math.sin(i));
+
+}
+}
